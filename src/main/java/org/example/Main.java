1635baab46c5c52b39cd830f5c3d911eb9741880
tumelomabena <tumelogmabena@gmail.com>
2026-02-26 13:12:27 +0200

    Added methods to add new clients and update existing clients

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -25,53 +25,6 @@ public class Main {
 
     }
 
-    private static void showTableInfo() {
-        String url = "jdbc:sqlite:clients.db";
-        String sql = "PRAGMA table_info(users)";
-
-        try (Connection conn = DriverManager.getConnection(url);
-             java.sql.Statement stmt = conn.createStatement();
-             java.sql.ResultSet rs = stmt.executeQuery(sql)) {
-
-            while (rs.next()) {
-                System.out.println(rs.getString("name") + " - " + rs.getString("type"));
-            }
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
-    }
-    private static void update() {
-        String url = "jdbc:sqlite:clients.db";
-        Scanner userin = new Scanner(System.in);
-        String sql = "UPDATE users SET name = ?," +
-                "email = ?, " +
-                "dob = ? " +
-                "WHERE id = ?";
-        System.out.println("which id would you like to edit");
-        int id = Integer.parseInt(userin.nextLine());
-
-        System.out.println("First Name");
-        String name = userin.nextLine();
-        System.out.println("email");
-        String email = userin.nextLine();
-        System.out.println("Enter date of birth (YYYY-MM-DD):");
-        Date dob = java.sql.Date.valueOf((userin.nextLine()));
-
-
-        try (Connection conn = DriverManager.getConnection(url);
-             java.sql.PreparedStatement pstmt = conn.prepareStatement(sql)) {
-            pstmt.setString(1, name);
-            pstmt.setString(2, email);
-            pstmt.setDate(3, dob);
-            pstmt.setInt(4, id);
-            pstmt.executeUpdate();
-            System.out.println("Data inserted.");
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-
-        }
-    }
-
     private static void createNewTable() {
         String url = "jdbc:sqlite:clients.db";
         String sql = "CREATE TABLE IF NOT EXISTS users(\n" //this will stop the code from creating multiple tables with the same name
@@ -89,6 +42,23 @@ public class Main {
             System.out.println(e.getMessage());//that get message will get an error message
         }
 
+
+    }
+
+    private static void connect() {
+        String url = "jdbc:sqlite:clients.db";
+
+        try (Connection conn = DriverManager.getConnection(url)) {
+            if (conn != null) {
+                System.out.println("Connected to database");
+
+            }
+
+        }
+        catch (SQLException e)
+        {
+            System.out.println(e.getMessage());
+        }
 
     }
 
@@ -134,6 +104,56 @@ public class Main {
         }
     }
 
+
+    private static void update() {
+        String url = "jdbc:sqlite:clients.db";
+        Scanner userin = new Scanner(System.in);
+        String sql = "UPDATE users SET name = ?," +
+                "email = ?, " +
+                "dob = ? " +
+                "WHERE id = ?";
+        System.out.println("which id would you like to edit");
+        int id = Integer.parseInt(userin.nextLine());
+
+        System.out.println("First Name");
+        String name = userin.nextLine();
+        System.out.println("email");
+        String email = userin.nextLine();
+        System.out.println("Enter date of birth (YYYY-MM-DD):");
+        Date dob = java.sql.Date.valueOf((userin.nextLine()));
+
+
+        try (Connection conn = DriverManager.getConnection(url);
+             java.sql.PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setString(1, name);
+            pstmt.setString(2, email);
+            pstmt.setDate(3, dob);
+            pstmt.setInt(4, id);
+            pstmt.executeUpdate();
+            System.out.println("Data inserted.");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+
+        }
+    }
+
+    private static void showTableInfo() {
+        String url = "jdbc:sqlite:clients.db";
+        String sql = "PRAGMA table_info(users)";
+
+        try (Connection conn = DriverManager.getConnection(url);
+             java.sql.Statement stmt = conn.createStatement();
+             java.sql.ResultSet rs = stmt.executeQuery(sql)) {
+
+            while (rs.next()) {
+                System.out.println(rs.getString("name") + " - " + rs.getString("type"));
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+
     private static void printOutData() {
         String url = "jdbc:sqlite:clients.db";
         String sql = "SELECT id, name, email, dob FROM users";
@@ -168,24 +188,5 @@ public class Main {
             System.out.println(e.getMessage());
 
         }
-    }
-
-
-
-    private static void connect() {
-        String url = "jdbc:sqlite:clients.db";
-
-        try (Connection conn = DriverManager.getConnection(url)) {
-            if (conn != null) {
-                System.out.println("Connected to database");
-
-            }
-
-        }
-        catch (SQLException e)
-        {
-            System.out.println(e.getMessage());
-        }
-
     }
 }
